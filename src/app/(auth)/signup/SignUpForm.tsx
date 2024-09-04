6bc261d1708dc80d7f03afa6d7741323e3f0fede
francis <clyderitchie@yahoo.com>
2024-09-03 18:33:21 -0700

    Added the finishing logic and styling of signUp page

--- src/app/(auth)/signup/SignUpForm.tsx
+++ src/app/(auth)/signup/SignUpForm.tsx
@@ -19,6 +19,8 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState, useTransition } from "react";
 import { signUp } from "./actions";
+import { PasswordInput } from "@/components/PasswordInput";
+import LoadingButton from "@/components/LoadingButton";
 
 export default function SignUpForm() {
   const [error, setError] = useState<string>();
@@ -79,15 +81,15 @@ export default function SignUpForm() {
             <FormItem>
               <FormLabel>Password</FormLabel>
               <FormControl>
-                <Input placeholder="Password" type="password" {...field} />
+                <PasswordInput placeholder="Password" {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit" className="w-full">
+        <LoadingButton loading={isPending} type="submit" className="w-full">
           Create account
-        </Button>
+        </LoadingButton>
       </form>
     </Form>
   );

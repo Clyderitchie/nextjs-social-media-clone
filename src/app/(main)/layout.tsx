e547d1a0d6111314fad686256964a6347e216b7e
francis <clyderitchie@yahoo.com>
2024-09-07 21:29:53 -0700

    Started navbar and added ability to logout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,15 +4,23 @@
 import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
 import SessionProvider from "./SessionProvider";
+import Navbar from "./Navbar";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await validateRequest(); // Fetches Session 
+  const session = await validateRequest(); // Fetches Session
 
   if (!session.user) redirect("/login"); // Checks if user is logged in
 
-  return <SessionProvider value={session}>{children}</SessionProvider>; // Makes Sessions content available to all children so you do not have to fetch there for it.
+  return (
+    <SessionProvider value={session}>
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        <div className="max-w-7xl mx-auto p-5">{children}</div>
+      </div>
+    </SessionProvider>
+  ); // Makes Sessions content available to all children so you do not have to fetch there for it.
 }

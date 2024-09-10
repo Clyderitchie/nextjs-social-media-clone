8e6b30c00db98afeb9c0e46d5f4d53e6ef1e9c6e
francis <clyderitchie@yahoo.com>
2024-09-09 19:12:48 -0700

    Added in the navbar and logic for it

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -1,3 +1,4 @@
+import SearchField from "@/components/SearchField";
 import UserButton from "@/components/UserButton";
 import Link from "next/link";
 
@@ -5,8 +6,11 @@ export default function Navbar() {
   return (
     <header className="sticky top-0 z-10 bg-card shadow-sm">
       <div className="max-w-7xl mx-auto flex items-center justify-center flex-wrap gap-5 px-5 py-3">
-        <Link href="/" className="text-2xl font-bold text-primary">Clone Site</Link>
-        <UserButton/>
+        <Link href="/" className="text-2xl font-bold text-primary">
+          Clone Site
+        </Link>
+        <SearchField />
+        <UserButton className="sm:ms-auto" />
       </div>
     </header>
   );

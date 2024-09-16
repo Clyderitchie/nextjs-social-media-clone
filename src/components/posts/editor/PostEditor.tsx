141f614e085ac355071bae3082ce5b2fdeb0e18d
francis <clyderitchie@yahoo.com>
2024-09-15 17:15:58 -0700

    Working on adding posts in

--- src/components/posts/editor/PostEditor.tsx
+++ src/components/posts/editor/PostEditor.tsx
@@ -7,15 +7,20 @@ import { submitPost } from "./actions";
 import UserAvatar from "@/components/UserAvatar";
 import { useSession } from "@/app/(main)/SessionProvider";
 import { Button } from "@/components/ui/button";
-import './styles.css';
+import "./styles.css";
 
 export default function PostEditor() {
-  const { user } = useSession();
+  const {user} = useSession();
 
   const editor = useEditor({
     extensions: [
-      StarterKit.configure({ bold: false, italic: false }),
-      PlaceHolder.configure({ placeholder: "What's over there?" }),
+      StarterKit.configure({
+        bold: false,
+        italic: false,
+      }),
+      PlaceHolder.configure({
+        placeholder: "This is holding its place for you",
+      }),
     ],
   });
 
@@ -24,29 +29,27 @@ export default function PostEditor() {
       blockSeparator: "\n",
     }) || "";
 
-  async function onSubmit() {
-    await submitPost(input);
+  async function onSubmit () {
+    await submitPost(input)
     editor?.commands.clearContent();
   }
 
-  return (
-    <div className="flex flex-col gap-5 rounded-2xl bg-card shadow-sm">
-      <div className="flex gap-5">
-        <UserAvatar avatarUrl={user.avatarUrl} className="hidden sm:inline" />
-        <EditorContent
-          editor={editor}
-          className="w-full max-h-[20rem] overflow-y-auto bg-background rounded-2xl px-5 py-3 "
-        />
-      </div>
-      <div className="flex justify-end">
-        <Button
-          onClick={onSubmit}
-          disabled={!input.trim()}
-          className="min-w-20"
-        >
-          Post
-        </Button>
-      </div>
+  return <div className="flex flex-col gap-5 rounded-2xl bg-card shadow-sm p-5">
+    <div className="flex gap-5">
+      <UserAvatar avatarUrl={user.avatarUrl} className="hidden sm:inline" />
+      <EditorContent 
+      editor={editor}
+      className="w-full max-h-[20rem] overflow-y-auto bg-background rounded-2xl px-5 py-3"
+      />
     </div>
-  );
+    <div className="flex justify-end gap-3">
+      <Button
+      onClick={onSubmit}
+      disabled={!input.trim()}
+      className="min-w-20"
+      >
+        Post
+      </Button>
+    </div>
+  </div>
 }
